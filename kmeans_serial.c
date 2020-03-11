384e1ed0abe9dcab6d09292107e0c4bb9c7a50cd
Nicolas Carolo <nicolas@AOD260.homenet.telecomitalia.it>
2020-03-11 01:10:52 -0400

    euclidean distance and first cluster assignment

--- kmeans_serial.c
+++ kmeans_serial.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <math.h>
 
 typedef struct {
     double x;
     double y;
+    int cluster_id;
 } Point;
 
 void print_points_array(Point *points_array, int n_line);
 int get_index_factor(int n_lines, int n_cluster);
+int assign_cluster(int point_index, Point *points_array, int *cluster_indexes, int n_clusters);
+double calc_euclidean_distance(Point point, Point cluster);
 
 
 
@@ -25,8 +28,8 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    int n_cluster = atoi(argv[2]);
-    int cluster_indexes[n_cluster];
+    int n_clusters = atoi(argv[2]);
+    int cluster_indexes[n_clusters];
 
     if (!(fin = fopen(argv[1], "r"))) {
         printf("the input file '%s' does not exist\n", argv[1]);
@@ -35,6 +38,7 @@ int main(int argc, char *argv[]){
 
     while (fgets(buf, sizeof(buf), fin)) {
 		sscanf(buf, "%lf %lf", &points_array[n_line].x, &points_array[n_line].y);
+        points_array[n_line].cluster_id = -1;
 		n_line++;
 		if (n_line > d) {
 			d = d + 2;
@@ -52,17 +56,25 @@ int main(int argc, char *argv[]){
 
     puts("\nClusters:");
     int i;
-    int base_index_factor = get_index_factor(n_line, n_cluster);
+    int base_index_factor = get_index_factor(n_line, n_clusters);
     int index_factor = 0;
-    for (i = 0; i < n_cluster; i++) {
+    for (i = 0; i < n_clusters; i++) {
         cluster_indexes[i] = index_factor;
         index_factor = index_factor + base_index_factor;
     }
 
 
-    for (i = 0; i < n_cluster; i++) {
-        printf("%d\n", cluster_indexes[i]);
+    for (i = 0; i < n_clusters; i++) {
+        printf("%d: (%lf, %lf)\n", cluster_indexes[i], points_array[cluster_indexes[i]].x, points_array[cluster_indexes[i]].y);
     }
+
+
+    for (i = 0; i < n_line; i++) {
+        points_array[i].cluster_id = assign_cluster(i, points_array, cluster_indexes, n_clusters);
+    }
+
+    puts("First cluster assignment:");
+    print_points_array(points_array, n_line);
 
 
     fclose(fin);
@@ -75,11 +87,31 @@ int main(int argc, char *argv[]){
 void print_points_array(Point *points_array, int n_line) {
     int i;
     for (i = 0; i < n_line; i++) {
-        printf("(%lf, %lf)\n", points_array[i].x, points_array[i].y);
+        printf("(%lf, %lf) assigned to cluster %d\n", points_array[i].x, points_array[i].y, points_array[i].cluster_id);
     }
 }
 
 
-int get_index_factor(int n_line, int n_cluster) {
-    return n_line / n_cluster;
+int get_index_factor(int n_line, int n_clusters) {
+    return n_line / n_clusters;
+}
+
+
+int assign_cluster(int point_index, Point *points_array, int *cluster_indexes, int n_clusters) {
+    int i;
+    double min_distance = calc_euclidean_distance(points_array[point_index], points_array[cluster_indexes[0]]);
+    int cluster_id = cluster_indexes[0];
+    for (i = 1; i < n_clusters; i++) {
+        double distance = calc_euclidean_distance(points_array[point_index], points_array[cluster_indexes[i]]);
+        if (distance < min_distance) {
+            min_distance = distance;
+            cluster_id = cluster_indexes[i];
+        }
+    }
+    return cluster_id;
+}
+
+
+double calc_euclidean_distance(Point point, Point cluster) {
+    return sqrt(pow((point.x - cluster.x), 2) + pow((point.y - cluster.y), 2));
 }
