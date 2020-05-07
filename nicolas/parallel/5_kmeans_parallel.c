46253c8973e06e5fc4ec3c26b8b9c8edfbaea96a
Nicolas Carolo <nicolas.carolo@outlook.com>
2020-05-07 10:25:43 +0200

    parallel test

--- nicolas/parallel/5_kmeans_parallel.c
+++ nicolas/parallel/5_kmeans_parallel.c
@@ -291,14 +291,25 @@ Point *calc_centroids(Point *points_array, int n_line, int n_clusters, int n_thr
     }
     */
 
+    int chunk;
+    /*if (N_COORDINATES % n_threads == 0) {
+        chunk = N_COORDINATES / n_threads;
+    } else {
+        chunk = (N_COORDINATES / n_threads) + 1;
+    }*/
+
     for (i = 0; i < n_line; i++) {
         centroid_index = points_array[i].cluster_id - 1;
-        for (j = 0; j < N_COORDINATES; j++) {
-            if (n_points[centroid_index] == 0) {
-                centroids[centroid_index].coordinate[j] = 0;
+        //#pragma omp parallel shared(centroids, chunk)
+        //{
+            //#pragma omp for schedule(static,chunk)
+            for (j = 0; j < N_COORDINATES; j++) {
+                if (n_points[centroid_index] == 0) {
+                    centroids[centroid_index].coordinate[j] = 0;
+                }
+                centroids[centroid_index].coordinate[j] += points_array[i].coordinate[j];
             }
-            centroids[centroid_index].coordinate[j] += points_array[i].coordinate[j];
-        }
+        //}
         n_points[centroid_index]++;
     }
 
@@ -308,7 +319,7 @@ Point *calc_centroids(Point *points_array, int n_line, int n_clusters, int n_thr
     }
     */    
 
-    int chunk;
+    //int chunk;
     if (N_COORDINATES % n_threads == 0) {
         chunk = N_COORDINATES / n_threads;
     } else {
