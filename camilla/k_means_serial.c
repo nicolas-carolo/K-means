965e2ecb036ca156e2e415c71aff2ba58b21d9fd
Camilla Modica <37158725+therealzuc@users.noreply.github.com>
2020-05-18 17:14:25 +0200

    Update k_means_serial.c

--- camilla/k_means_serial.c
+++ camilla/k_means_serial.c
@@ -5,45 +5,37 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-#define k 10
-#define number_points 49548
-#define number_coordinates 115
+#define N_COORDINATES 115
 
 struct centroid
 {
-    double coordinates_centroid[number_coordinates];
-    double coordinates_previous_centroid[number_coordinates];
+    double coordinates_centroid[N_COORDINATES];
+    double coordinates_previous_centroid[N_COORDINATES];
 };
-
-struct centroid centroids[k];
 
 struct point
 {
     int name_centroid;
-    double coordinates_point[number_coordinates];
+    double coordinates_point[N_COORDINATES];
 };
 
-struct point points[number_points];
-
-void centroid_void(struct centroid *c, int d, int i)
+void centroid_void(struct centroid *c, int d, int i, int k, int n_row)
 {
     c[d].coordinates_centroid[i]= c[d].coordinates_previous_centroid[i];
 }
 
-int euclidean_distance(double *coordinates, struct centroid *c)
+int euclidean_distance(double *coordinates, struct centroid *c, int k, int n_row)
 {
     int cluster;
     double minimum;
-    double coord_point[number_coordinates];
+    double coord_point[N_COORDINATES];
     double distance_point;
     
     for (int d = 0; d<k; d++)
     {
         distance_point = 0;
-        for (int i = 0; i<number_coordinates; i++)
+        for (int i = 0; i<N_COORDINATES; i++)
         {
-            //printf("%lf\n", coordinates[i]);
-            //printf("%lf\n", c[d].coordinates_centroid[i]);
             distance_point += pow((coordinates[i] - c[d].coordinates_centroid[i]), 2);
         }
         if (d == 0)
@@ -57,141 +49,168 @@ int euclidean_distance(double *coordinates, struct centroid *c)
             minimum = distance_point;
             cluster = d;
         }
-        //printf("distance %lf\n", distance_point);
-        //printf("minimum %lf\n", minimum);
-        //printf("%d\n", cluster);
     }
-    //printf("%d\n", cluster);
     return cluster;
 }
 
-void mean_coordinates(double *coordinates, struct point *p, int d)
+void mean_coordinates(double *coordinates, struct centroid *c, struct point *p, int d, int k, int n_row)
 {
-    double mean[number_coordinates] = {0};
+    double mean[N_COORDINATES] = {0};
     int count = 0;
-    for (int row = 0; row<number_points; row++)
+    for (int row = 0; row<n_row; row++)
     {
         if (p[row].name_centroid == d)
         {
-            for (int i = 0; i<number_coordinates; i++)
+            for (int i = 0; i<N_COORDINATES; i++)
             {
                 mean[i] += p[row].coordinates_point[i];
-                //printf("maoooooooooooooooooooooooooooo %lf\n", mean[i]);
             }
             count++;
         }
     }
     if (count != 0)
     {
-        for (int i = 0; i<number_coordinates; i++)
+        for (int i = 0; i<N_COORDINATES; i++)
         {
-            //printf("maoooooooooooooooooooooooooooo %lf\n", mean[i]);
-            //printf("%d\n", count);
             coordinates[i] = mean[i]/count;
-            //printf("provaaaaaaaaaaaaa %lf\n", coordinates[i]);
         }
     } else {
-        for(int i = 0; i<number_coordinates; i++)
+        for(int i = 0; i<N_COORDINATES; i++)
         {
-            printf("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR\n");
-            centroid_void(centroids, d, i);
+            centroid_void(c, d, i, k, n_row);
         }
     }
 }
 
-int difference_centroids(struct centroid *c)
+int difference_centroids(struct centroid *c, int k, int n_row)
 {
     double difference = 0;
     int count = 0;
     for (int d = 0; d<k; d++)
     {
         difference = 0;
-        for (int i = 0; i<number_coordinates; i++)
+        for (int i = 0; i<N_COORDINATES; i++)
         {
             difference += pow((c[d].coordinates_centroid[i] - c[d].coordinates_previous_centroid[i]), 2);
         }
         difference = sqrt(difference);
-        //printf("ddddddd %lf\n", difference);
         if (difference > 0.001)
         {
             count ++;
         }
     }
-    //printf("cccccc %d\n", count);
-    //return count;
     return count;
 }
 
-void selection_centroid(struct point *p, struct centroid *c)
+void selection_centroid(struct point *p, struct centroid *c, int k, int n_row)
 {
-  int count = number_points/k;
-  //printf("%d\n", count);
+  int count = n_row/k;
 
   for(int d = 0; d<k; d++)
   {
-      for(int row = 0; row<number_points; row++)
+      for(int row = 0; row<n_row; row++)
       {
-          for(int i = 0; i<number_coordinates; i++)
+          for(int i = 0; i<N_COORDINATES; i++)
           {
               if(row == (d*count))
               {
                   c[d].coordinates_centroid[i] = p[row].coordinates_point[i];
-                  //printf("bbbbbbbbbbbbbbbbbbb %lf\n", c[d].coordinates_centroid[i]);
               }
           }
       }
   }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     clock_t t;
+    int d = 10;
+    int index_1 = 0;
+    int index_2 = 0;
+    int index_3 = 0;
+    int n_row = 0;
+    char row[20000];
+    char string[N_COORDINATES][512];
 
     //load the dataset
     FILE *stream;
-    stream = fopen("dataset6.txt", "r");
 
-    if ((stream = fopen("dataset6.txt", "r")) == NULL)
-    {
-        printf("File can not be opened!");
-        exit(1);
+    if(argc != 3){
+        printf("error\n");
+        return 1;
     }
 
-    stream = fopen("dataset6.txt", "r");
+    int k = atoi(argv[2]);
+
+    //structs
+    struct centroid centroids[k];
+    struct point *points = malloc(d * sizeof(struct point));
+
+    if ((stream = fopen(argv[1], "r")) == NULL)
+    {
+        printf("File can not be opened!");
+        return -1;
+    }
 
     t = clock();
 
     //store the values of each point
-    for (int row = 0; row<number_points; row++)
+
+    while(fgets(row, sizeof(row), stream) != NULL)
     {
-        for (int i = 0; i<number_coordinates; i++)
-        {
-            fscanf(stream, "%lf,", &points[row].coordinates_point[i]);
-            //printf("%lf\n", points[row].coordinates_point[i]);
+        index_2 = 0;
+        index_3 = 0;
+
+        for (index_1 = 0; index_1 <= strlen(row); index_1++){
+            if (row[index_1] == ',' || row[index_1] == '\0'){
+                string[index_3][index_2] = '\0';
+                index_3++;
+                index_2 = 0;
+            } else {
+                string[index_3][index_2] = row[index_1];
+                index_2++;
+            }
+        }
+
+        for(index_1 = 0; index_1<index_3; index_1++){
+            points[n_row].coordinates_point[index_1] = atof(string[index_1]);
+        }
+
+        points[n_row].name_centroid = -1;
+
+        n_row++;
+
+        if (n_row +1 > d){
+            d = d + 1;
+            points = realloc(points, d * sizeof(struct point));
+            if(points == NULL){
+                printf("Error!\n");
+                return 1;
+            }
         }
     }
 
     fclose(stream);
 
     //choose the centroids
-    selection_centroid(points, centroids);
+    selection_centroid(points, centroids, k, n_row);
     
     //loop with euclidean distance and new centroids
     int check = 0;
     int n_iterations = 0;
     do {
-        for (int row = 0; row<number_points; row++)
+        for (int row = 0; row<n_row; row++)
         {
-            points[row].name_centroid = euclidean_distance(points[row].coordinates_point, centroids);
-            //printf("new name %d\n", points[row].name_centroid);
+            points[row].name_centroid = euclidean_distance(points[row].coordinates_point, centroids, k, n_row);
+
         }
         for (int d=0; d<k; d++)
         {
             memcpy (centroids[d].coordinates_previous_centroid, centroids[d].coordinates_centroid, sizeof(centroids[d].coordinates_centroid));
-            mean_coordinates(centroids[d].coordinates_centroid, points, d);
+            mean_coordinates(centroids[d].coordinates_centroid, centroids, points, d, k, n_row);
         }
-        check = difference_centroids(centroids);
+        check = difference_centroids(centroids, k, n_row);
         n_iterations++;
-        printf("ooooooooooooooo %d\n", n_iterations);
+        printf("number of iterations: %d\n", n_iterations);
     }
     while (check != 0);
 
@@ -199,21 +218,20 @@ int main() {
     t = clock() - t;
 
     double time_taken = ((double)t/CLOCKS_PER_SEC);
-    printf("timeeeeeeeee %lf\n", time_taken);
+    printf("time: %lf\n", time_taken);
 
     for (int d = 0; d<k; d++)
     {
         printf("\n");
-        for (int i = 0; i<number_coordinates; i++)
+        for (int i = 0; i<N_COORDINATES; i++)
         {
-            printf("centroide %lf\n", centroids[d].coordinates_centroid[i]);
+            printf("centroid: %lf\n", centroids[d].coordinates_centroid[i]);
         }
    
-        for (int row = 0; row<number_points; row++)
+        for (int row = 0; row<n_row; row++)
         {
             if (points[row].name_centroid == d)
             {
-            //printf("%lf %lf\n", points[row].coordinates_point[0], points[row].coordinates_point[1]);
         }
         }
     }
